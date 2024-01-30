21390c88977551178080bf691e84c03d0c5d3a4f
anthony-mini <anthony-mini@outlook.fr>
2024-01-30 22:19:32 +0100

    Add logo and pokeball assets, update base styles, and refactor pagination styles

---
+++ src/assets/index.tsx
@@ -0,0 +1,2 @@
+export { default as logo } from './pokemon-23.svg';
+export { default as pokeball } from './pokeball.svg';
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
-// import Logo from '../../assets/logo.svg';
+import { logo, pokeball } from '../../assets/index';
 
 const Header = () => {
   return (
     <>
-      <header className="bg-gray-800 text-white py-4 px-6" id="header">
-        <Link to="/">
-          <h1 className="text-3xl font-bold">Welcome to React Courses !</h1>
+      <header
+        className="fixed w-full bg-gray-600 py-6 px-4 shadow-lg flex justify-center items-center relative"
+        id="header">
+        <img
+          src={pokeball}
+          alt="pokeball"
+          className="absolute z-10 w-100 opacity-70"></img>
+        <Link to="/" className="z-20">
+          <img src={logo} alt="logo" className="w-80" />
         </Link>
       </header>
     </>
