bd7f63e145dbf7f544f7d5ebc5af5eced0655783
anthony-mini <anthony-mini@outlook.fr>
2024-02-12 23:18:08 +0100

    Ajouter la fonctionnalité de recherche multi-sélection de pokémon

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -3,3 +3,4 @@ export { default as Button } from './Buttons';
 export { default as Select } from './Select';
 export { default as Grid } from './Grid';
 export { default as Pagination } from './Pagination';
+export { default as MultiSearchComponent } from './Multisearch';
