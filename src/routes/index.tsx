8641c01b3447b02fd2cb7f9e3b8231d9d9fb64eb
anthony-mini <anthony-mini@outlook.fr>
2024-02-13 10:27:54 +0100

    Ajouter la grille de composants et les détails du Pokémon

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { Error, Home } from '../pages';
+import { Error, Home, PokemonDetail } from '../pages';
 
 function App() {
   return (
@@ -8,6 +8,10 @@ function App() {
       <Routes>
         <Route path="/" element={(<Home />) as React.ReactNode} />
         <Route path="*" element={(<Error />) as React.ReactNode} />
+        <Route
+          path="/pokemon/:id"
+          element={(<PokemonDetail />) as React.ReactNode}
+        />
       </Routes>
     </Router>
   );
